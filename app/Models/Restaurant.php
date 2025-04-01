601d1261826beedbcd9d0200c79b7dfec1be9e61
lahcen <lahcenwirghanec11@gmail.com>
2025-04-01 12:00:58 +0000

    staging changes to fix some modules problems

---
+++ app/Models/Restaurant.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Restaurant extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'description',
+        'address',
+        'city',
+        'postal_code',
+        'phone',
+        'email',
+        'website',
+        'opening_time',
+        'closing_time',
+        'opening_days',
+        'user_id',
+        'cover_image',
+        'is_active',
+        'max_booking_days_ahead',
+    ];
+
+    protected $casts = [
+        'opening_days' => 'array',
+        'is_active' => 'boolean',
+        'opening_time' => 'datetime',
+        'closing_time' => 'datetime',
+        'max_booking_days_ahead' => 'integer',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function bookings()
+    {
+        return $this->hasMany(Reservation::class);
+    }
+
+    public function tables()
+    {
+        return $this->hasMany(Table::class);
+    }
+
+    public function reviews()
+    {
+        return $this->hasMany(Review::class);
+    }
+
+    public function images()
+    {
+        return $this->hasMany(RestaurantImage::class);
+    }
+}
+
+
