2fdd16cccc1cde56d6d5edb82e8f8cc7369b24d3
lahcen <lahcenwirghanec11@gmail.com>
2025-04-24 23:59:36 +0000

    resetup pusher

--- resources/js/echo.js
+++ resources/js/echo.js
@@ -7,5 +7,11 @@ window.Echo = new Echo({
     broadcaster: 'pusher',
     key: import.meta.env.VITE_PUSHER_APP_KEY,
     cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER,
-    forceTLS: true
+    forceTLS: true,
+    auth: {
+        headers: {
+            'X-CSRF-TOKEN': document.querySelector('meta[name="csrf-token"]')?.getAttribute('content')
+        }
+    }
 });
+console.log('Echo initialized with key:', import.meta.env.VITE_PUSHER_APP_KEY);
